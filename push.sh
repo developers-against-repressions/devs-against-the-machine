d855c5a66f0008c08956c34a10abd23e09f8acb3
Ilia Liubimov <ferselinc@gmail.com>
2019-11-29 14:33:59 +0300

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "bot@github.com"
+  git config --global user.name "GitHub Bot"
+}
+
+git_commit_readme() {
+  git add README.md
+  git commit --message "README.md updated"
+}
+
+git_push() {
+  git checkout -b master
+  git remote remove origin
+  git remote add origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+  git push origin HEAD
+}
+
+setup_git
+git_commit_readme
+git_push
